0d1c05b258c3994f07777e2f41c4945dc3f542a4
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-09-01 16:20:13 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
-let a = [8, 2, 2, 5] => arr = [8, 2, 5];
-let filterArray = a.filter(function(id, ex) {
-    return a.indexOf(id) == ex;
-})
+const modal = document.getElementById('myModal');
+const btnOpen = document.getElementById("openModal");
+const btn = document.querySelector('.button');
+const clickCount = document.querySelector('.click-counter');
+let button = document.querySelector(".btn");
+
+
+btnOpen.onclick = function() {
+    if (modal.style.display === "block") {
+        modal.style.display = "none";
+    } else {
+        modal.style.display = "block";
+    }
+}
+  
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+
+let clicks = 0;
+
+btn.addEventListener("dblclick", () => {
+    clicks = 0;
+    clickCount.textContent = clicks;
+});
+
+btn.addEventListener("click", () => {
+    clicks++;
+    clickCount.textContent = clicks;
+});
+
+let rotation = 0;
+button.addEventListener('click', () => {
+    const targetX = window.screenX + 100; 
+      const targetY = window.screenY + 100; 
+      modal.style.left = `${targetX}px`;
+      modal.style.top = `${targetY}px`;
+  });
